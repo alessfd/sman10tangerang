53e3b20b6d03bb36338d05796ce018361d4c076c
C0uler <jordan.arya@student.umn.ac.id>
2024-11-15 13:56:11 +0700

    update navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,9 +85,9 @@
             <!-- Sidebar Section -->
             <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
 
-                <div class="w-full bg-white shadow flex flex-col my-4 p-6">
-                    <p class="text-xl font-semibold pb-5">About Us</p>
-                    <p class="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
+                <div class="w-full bg-white shadow flex flex-col my-4 p-6 text-justify" style=" text-justify: inter-word; hyphens: auto;">
+                    <p class="text-xl font-semibold pb-5" >About Us</p>
+                    <p class="pb-2">SMAN 10 Tangerang Selatan, berdiri sejak tahun 2006 dengan terakreditasi "A" adalah sekolah unggulan yang mengutamakan keunggulan akademik serta pengembangan karakter siswa. Dilengkapi dengan fasilitas modern, seperti laboratorium sains dan komputer, perpustakaan, lapangan olahraga, dan kantin, kami juga menawarkan berbagai ekstrakurikuler, termasuk tim olahraga, klub seni, dan kelompok sains. Dengan visi mencetak lulusan berprestasi yang siap berkontribusi positif bagi masyarakat.</p>
                     <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
                         Get to know us
                     </a>
